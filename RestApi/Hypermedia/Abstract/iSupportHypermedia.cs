18acb4907780aaefc116235c96aab43ed9f950e6
Guilherme Nascimento <guiguidefarias@hotmail.com>
2021-05-26 12:32:00 -0300

    Criado as interfaces e Constantes

---
+++ RestApi/Hypermedia/Abstract/iSupportHypermedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestApi.Hypermedia.Abstract
+{
+    public interface iSupportHypermedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
