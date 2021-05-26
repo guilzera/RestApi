6664b81513d3886737d7664ecb3a6c584c35fcbf
Guilherme Nascimento <guiguidefarias@hotmail.com>
2021-05-26 13:01:03 -0300

    Implementado a Classe HyperMediaLink

--- RestApi/Hypermedia/Abstract/iSupportHypermedia.cs
+++ RestApi/Hypermedia/Abstract/iSupportHypermedia.cs
@@ -2,7 +2,7 @@
 
 namespace RestApi.Hypermedia.Abstract
 {
-    public interface iSupportHyperMedia
+    public interface ISupportHyperMedia
     {
         List<HyperMediaLink> Links { get; set; }
     }
---
+++ RestApi/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel { get; set; }
+        public string href;
+        private string Href
+        {
+            get
+            {
+                object _lock = new object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set
+            {
+                href = value;
+            }
+        }
+        public string Type { get; set; }
+        public string Action { get; set; }
+    }
+}
